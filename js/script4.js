ef1d7bfb1ee78921c414e65101567e159390609d
HannaSoloviova <49920401+HannaSoloviova@users.noreply.github.com>
2020-01-12 22:30:54 +0200

    Update script4.js

--- js/script4.js
+++ js/script4.js
@@ -1,9 +1,10 @@
-let digit1 = parseInt(prompt('Введите число'));
+let digit = parseInt(prompt('Введите число'));
+let lastDigit = digit % 10;
 
-if (digit1 % 2 === 0) {
+if (digit % 2 === 0) {
 	console.log('Число заканчивается четной цифрой');
 } else { 
 	console.log('Число заканчивается нечетной цифрой');
 }
 
-console.log(digit1 % 10);
+console.log(lastDigit);
